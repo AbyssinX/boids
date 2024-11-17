e8d375e7a76e8dd0133b3dced5fa348b69ef9dd1
AbyssinX <alxleyb@gmail.com>
2024-11-17 22:42:48 +0000

    Tweaked alignment parameter

--- boids/app/Main.hs
+++ boids/app/Main.hs
@@ -248,7 +248,7 @@ separationForce boid others =
 alignmentForce :: Boid -> [Boid] -> Force
 alignmentForce boid boids =
     if count > 0
-    then (avgVel ^-^ vel boid) ^* scalealignmen  -- Scale alignment effect
+    then (avgVel ^-^ vel boid) ^* scalealignment  -- Scale alignment effect
     else V2 0 0
   where
     -- Gather velocities of nearby boids within alignmentDistance
@@ -365,8 +365,8 @@ detectCollisions quadTree boid depth
 -- Parameters
 --------------------------------------
 
-scalealignmen :: Float
-scalealignmen = 0.2
+scalealignment :: Float
+scalealignment = 0.1
 
 visualRange:: Float
 visualRange = 0.6
